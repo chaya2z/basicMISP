7ff23dc404fdb0b030eeb5bea9862e5569d35fdb
chaya2z <itunano57@gmail.com>
2020-05-27 22:38:59 +0900

    ADD: registers
    ADD: $zero and $s0-$s7

---
+++ MIPS/mipsBasicCalc.py
@@ -0,0 +1,9 @@
+REGISTER_CONST_ZERO = 0
+register_tmp_store_s0 = 0
+register_tmp_store_s1 = 0
+register_tmp_store_s2 = 0
+register_tmp_store_s3 = 0
+register_tmp_store_s4 = 0
+register_tmp_store_s5 = 0
+register_tmp_store_s6 = 0
+register_tmp_store_s7 = 0
