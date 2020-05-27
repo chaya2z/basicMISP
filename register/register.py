50ee940d030985a97c093ae03a299d86520184b1
chaya2z <itunano57@gmail.com>
2020-05-27 23:34:11 +0900

    ADD: register.py
    ADD: MIPS all registers

--- register/register.py
+++ register/register.py
@@ -1,4 +1,30 @@
-REGISTER_CONST_ZERO = 0
+# 定数の0
+REGISTER_ZERO = 0
+
+# アセンブラの一時変数
+register_asm_tmp_at = 0
+
+# 式の評価と関数の結果
+register_v0 = 0
+register_v1 = 0
+
+# 引数
+register_a0 = 0
+register_a1 = 0
+register_a2 = 0
+register_a3 = 0
+
+# 一時変数
+register_t0 = 0
+register_t1 = 0
+register_t2 = 0
+register_t3 = 0
+register_t4 = 0
+register_t5 = 0
+register_t6 = 0
+register_t7 = 0
+
+# 一時変数（保存）
 register_tmp_store_s0 = 0
 register_tmp_store_s1 = 0
 register_tmp_store_s2 = 0
@@ -7,3 +33,23 @@ register_tmp_store_s4 = 0
 register_tmp_store_s5 = 0
 register_tmp_store_s6 = 0
 register_tmp_store_s7 = 0
+
+# 一時変数
+register_t8 = 0
+register_t9 = 0
+
+# OSカーネル用に予約
+register_k0 = 0
+register_k1 = 0
+
+# グローバル・ポインタ
+register_gp = 0
+
+# スタック・ポインタ
+register_sp = 0
+
+# フレーム・ポインタ
+register_fp = 0
+
+# 戻りアドレス
+register_ra = 0
