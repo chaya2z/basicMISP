6140286cf1a542399e096ae91305db9eb7c968cc
chaya2z <itunano57@gmail.com>
2020-05-28 20:30:38 +0900

    Add: program options
    Add: calculation mode and machine-lang mode option

--- parse/getCliArgs.py
+++ parse/getCliArgs.py
@@ -5,23 +5,22 @@ parser = argparse.ArgumentParser(
     description='Parse MIPS assembly',
     usage='python3 %(prog)s [file] [options]')
 
-# どこからでも呼び出せるヘルプ
-parser.add_argument('--version', '-v',
+# バージョン情報
+parser.add_argument('-v', '--version',
                     action='version',
                     version='%(prog)s β',
                     help='output version information and exit')
-
+# 計算モード（デフォルト）
+parser.add_argument('-c', '--calc',
+                    action='store_true',
+                    help='output calculation results')
+# 機械語モード
+parser.add_argument('-m', '--machine-lang',
+                    action='store_true',
+                    help='output machine language')
 # 引数を解析する
 args = parser.parse_args()
-parser.print_help()
 
-# arg_path = sys.argv
-# print(arg_path[1])
-#
-# # 引数の指定エラー
-# # ヘルプを表示
-# if len(arg_path) != 2:
-#     print("ERROR")
 
 # path 調べてそのファイルがちゃんと存在するか確認
 
