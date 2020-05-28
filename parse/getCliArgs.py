3bbcc523f3c78e60b7638e5865de142668f38a93
chaya2z <itunano57@gmail.com>
2020-05-29 00:45:50 +0900

    Remove: stopped specifying arg num
    Remove: nargs=1 make 1 item list

--- parse/getCliArgs.py
+++ parse/getCliArgs.py
@@ -7,8 +7,8 @@ def args_parser():
         description='Parse MIPS assembly',
         usage='python3 %(prog)s [file] [options]')
     # ファイルパスの受取
+    # nargs=1指定するとリスト型になるのでやめる
     parser.add_argument('asm_file',
-                        nargs=1,
                         type=argparse.FileType('r'),
                         help='path to target MIPS assembly file')
     # バージョン情報
