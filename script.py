7ca3488a906c601327f1b60a894c919da3729172
chaya2z <itunano57@gmail.com>
2020-05-29 02:02:27 +0900

    Rename: change file name
    Rename: asm_parser.py from parse.py & cli_args_parser.py from getCliArgs.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
-from parse import getCliArgs
+from parse import cli_args_parser
 
 
 def main():
-    args = getCliArgs.args_parser()
+    args = cli_args_parser.args_parser()
     target_path = args.asm_file
     calc_mode = args.calc
     machine_lang_mode = args.machine_lang
