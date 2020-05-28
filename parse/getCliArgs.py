3ac5af600e1f8423499f862a4c9392bb48af535d
chaya2z <itunano57@gmail.com>
2020-05-28 23:28:32 +0900

    Add: args_parser() func
    Add: define func

--- parse/getCliArgs.py
+++ parse/getCliArgs.py
@@ -1,26 +1,29 @@
 import argparse
 
-# パーサの作成
-parser = argparse.ArgumentParser(
-    description='Parse MIPS assembly',
-    usage='python3 %(prog)s [file] [options]')
-# ファイルパスの受取
-parser.add_argument('asm_file',
-                    nargs=1,
-                    type=argparse.FileType('r'),
-                    help='path to target MIPS assembly file')
-# バージョン情報
-parser.add_argument('-v', '--version',
-                    action='version',
-                    version='%(prog)s version: β',
-                    help='output version information and exit')
-# 計算モード（デフォルト）
-parser.add_argument('-c', '--calc',
-                    action='store_true',
-                    help='output calculation results (default mode)')
-# 機械語モード
-parser.add_argument('-m', '--machine-lang',
-                    action='store_true',
-                    help='output machine language')
-# 引数を解析する
-args = parser.parse_args()
+
+def args_parser():
+    # パーサの作成
+    parser = argparse.ArgumentParser(
+        description='Parse MIPS assembly',
+        usage='python3 %(prog)s [file] [options]')
+    # ファイルパスの受取
+    parser.add_argument('asm_file',
+                        nargs=1,
+                        type=argparse.FileType('r'),
+                        help='path to target MIPS assembly file')
+    # バージョン情報
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        version='%(prog)s version: β',
+                        help='output version information and exit')
+    # 計算モード（デフォルト）
+    parser.add_argument('-c', '--calc',
+                        action='store_true',
+                        help='output calculation results (default mode)')
+    # 機械語モード
+    parser.add_argument('-m', '--machine-lang',
+                        action='store_true',
+                        help='output machine language')
+    # 引数を解析する
+    args = parser.parse_args()
+    return args
