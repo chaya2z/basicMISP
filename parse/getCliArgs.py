61a2245bde67f64ecaac8d16287190ba38a1312d
chaya2z <itunano57@gmail.com>
2020-05-28 20:18:18 +0900

    Add: parser args
    Add: import argparse. Show program version.

--- parse/getCliArgs.py
+++ parse/getCliArgs.py
@@ -1,12 +1,27 @@
-import sys
+import argparse
 
-arg_path = sys.argv
-print(arg_path[1])
+# パーサの作成
+parser = argparse.ArgumentParser(
+    description='Parse MIPS assembly',
+    usage='python3 %(prog)s [file] [options]')
 
-# 引数の指定エラー
-# ヘルプを表示
-if len(arg_path) != 2:
-    print("ERROR")
+# どこからでも呼び出せるヘルプ
+parser.add_argument('--version', '-v',
+                    action='version',
+                    version='%(prog)s β',
+                    help='output version information and exit')
+
+# 引数を解析する
+args = parser.parse_args()
+parser.print_help()
+
+# arg_path = sys.argv
+# print(arg_path[1])
+#
+# # 引数の指定エラー
+# # ヘルプを表示
+# if len(arg_path) != 2:
+#     print("ERROR")
 
 # path 調べてそのファイルがちゃんと存在するか確認
 
