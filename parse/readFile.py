a76ceefe8e7838c65ac1b10166d632a6f9b9a34d
chaya2z <itunano57@gmail.com>
2020-05-28 18:49:44 +0900

    Add: readFile.py
    Add: get target file-path from arg

---
+++ parse/readFile.py
@@ -0,0 +1,10 @@
+import sys
+
+arg_path = sys.argv
+print(arg_path[1])
+
+# path 調べてそのファイルがちゃんと存在するか確認
+
+# with open(path) as f:
+#     target_assembly = [s.strip() for s in f.readlines()]
+#     print(target_assembly)
