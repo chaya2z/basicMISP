63f7911d4be9b3769bb5200ff887bc067c2efe4c
chaya2z <itunano57@gmail.com>
2020-05-29 23:02:10 +0900

    Fix: remove .strip(',')

--- parse/asm_parser.py
+++ parse/asm_parser.py
@@ -1,5 +1,5 @@
 def asm_parser(target_file):
-    instruction_list = [s.strip(',').split() for s in target_file.readlines()]
+    instruction_list = [s.split() for s in target_file.readlines()]
     return instruction_list
 
 
