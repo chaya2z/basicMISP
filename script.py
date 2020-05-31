ae34a9aecf0ebd670d55e0dcdebcd4aee7ba2331
chaya2z <itunano57@gmail.com>
2020-06-01 03:04:20 +0900

    Add; comment
    Reason: "file" is ambiguous.

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ from assembler import mips_assembler
 
 def main():
     args = cli_args_parser.args_parser()
-    target_path = args.file
+    target_path = args.file  # The assembly file specified on the command line
     test = asm_parser.asm_parser(target_path)
     print(test)
     print(mips_assembler.mips_assembler(test))
